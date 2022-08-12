b7a86bf92426b4bef07c7791de5b0472510766f3
yuuki kurose <pw1006@icloud.com>
2022-08-12 14:32:30 +0900

    テキストボックスに双方向データバインディングを設定、FormsModuleをインポート

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }         from '@angular/core';
 import { BrowserModule }    from '@angular/platform-browser';
+import { FormsModule }      from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent }     from './app.component';
@@ -12,10 +13,12 @@ import { HeroesComponent }  from './heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
 
