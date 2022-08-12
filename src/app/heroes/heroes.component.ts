93e6eb2e1a34a79d90caeafd4afa4cc18e9ddcae
yuuki kurose <pw1006@icloud.com>
2022-08-12 12:24:32 +0900

    ヒーローの名前を表示する為、プロパティ定義とテンプレートへバインディングする(一部インデント修正)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selectorは差し込む際のタグの名前
   selector:     'app-heroes',
   templateUrl:  './heroes.component.html',
   styleUrls:    ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  // ヒーローの名前を表示する為、heroプロパティを追加する
+  hero = "Windstorm";
 
   constructor() { }
 
