e8613233bce6d94610ebd51b054f3816aab95094
yuuki kurose <pw1006@icloud.com>
2022-08-12 10:52:55 +0900

    メインタイトルの変更、メインスタイルの設定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular_tour_of_heroes2';
+  title = 'Tour of Heroes2';
 }
