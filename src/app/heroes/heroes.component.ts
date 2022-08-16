e8726895678a6c4eebc9af695044c54e8b88ecbf
yuuki kurose <pw1006@icloud.com>
2022-08-16 17:45:02 +0900

    (新しい機能)
    feat : ユーザーがヒーローを選択すると選択されたヒーローIDが送信される

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit }  from '@angular/core';
+
 import { Hero }               from '../hero';
 import { HeroService }        from '../hero.service';
+import { MessageService }     from '../message.service'; 
 
 @Component({
   // selectorは差し込む際のタグの名前
@@ -15,31 +17,34 @@ export class HeroesComponent implements OnInit {
   
   // selectedHeroプロパティを定義
   selectedHero?: Hero;
-
-  // 選択されたヒーローが(idとname)selectedHeroプロパティに代入される
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  /*
-   * サービスからデータを取得するということは、
-   * サービスにあるgetHeroes()を使用し取得するということ
-   * heroesプロパティには[]が代入されているので型はいらない
-   */ 
-  getHeroes() :void {
-    this.heroService.getHeroes()  
-     //Observable(川)から流れてきた値(value)をsubscribe(購読)する
-     .subscribe(heroes => this.heroes = heroes);
-  }
    
   /*
    * インスタンス生成時にプライベートなプロパティとして定義され、
    * 同時にheroServiceを注入する場所として認識される
    */
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService, private messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
     this.getHeroes();
+  }
+
+   // 選択されたヒーローが(idとname)selectedHeroプロパティに代入される
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${ hero.id }`);
+  }
+  
+   /*
+   * サービスからデータを取得するということは、
+   * サービスにあるgetHeroes()を使用し取得するということ
+   * heroesプロパティには[]が代入されているので型はいらない
+   */
+  getHeroes() :void {
+    this.heroService.getHeroes()
+     //Observable(川)から流れてきた値(value)をsubscribe(購読)する
+     .subscribe(heroes => this.heroes = heroes);
   }
 }
 
