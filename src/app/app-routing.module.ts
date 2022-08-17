b9dbfd0bd8eb8099894175c4a3a798252d85e1a5
yuuki kurose <pw1006@icloud.com>
2022-08-17 14:45:40 +0900

    2022/8/17/14:47
    feat :  ダッシュボードのリンクを付ける為、トップシェルにリンクを設定
    　　　　ヒーローの詳細のURLに遷移できるようにする為、app-routing.module.tsにリン        クを追加
            ヒーロー詳細のページにダイレクトで遷移させる為、親子間の伝達を切る

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HeroesComponent }      from './heroes/heroes.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }  
+  // デフォルトルートを追加する　リダイレクトは新URLに移動させること	
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, 
+  { path: 'heroes',      component: HeroesComponent },  
+  { path: 'dashboard',   component: DashboardComponent },
+  { path: 'detail/: id', component: HeroDetailComponent },
 ];
 
 
