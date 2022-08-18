656dd52580e6beffba7d10d0c2962940b91c508f
yuuki kurose <pw1006@icloud.com>
2022-08-18 11:51:52 +0900

    feat: InMemoryWebAPIモジュールのインストール
    サーバーとの通信の再現の為、InMemoryWebAPIモジュールをインストール

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { NgModule }            from '@angular/core';
-import { BrowserModule }       from '@angular/platform-browser';
-import { FormsModule }         from '@angular/forms';
-import { AppRoutingModule }    from './app-routing.module';
-import { HttpClientModule }    from '@angular/common/http';
+import { NgModule }                       from '@angular/core';
+import { BrowserModule }                  from '@angular/platform-browser';
+import { FormsModule }                    from '@angular/forms';
+import { AppRoutingModule }               from './app-routing.module';
+import { HttpClientModule }               from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
-import { AppComponent }        from './app.component';
-import { HeroesComponent }     from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessageComponent }    from './message/message.component';
-import { DashboardComponent }  from './dashboard/dashboard.component';
+import { InMemoryDataService }            from './in-memory-data.service';
+
+import { AppComponent }                   from './app.component';
+import { HeroesComponent }                from './heroes/heroes.component';
+import { HeroDetailComponent }            from './hero-detail/hero-detail.component';
+import { MessageComponent }               from './message/message.component';
+import { DashboardComponent }             from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +26,9 @@ import { DashboardComponent }  from './dashboard/dashboard.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService,{ dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
