871def026e0b1ddffc28a151a8a0f65b4852f5c2
yuuki kurose <pw1006@icloud.com>
2022-08-18 11:23:00 +0900

    サーバーからデータの取得
    feat : サーバーとの通信を利用する為、HttpClientModuleをインポートする

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }            from '@angular/core';
 import { BrowserModule }       from '@angular/platform-browser';
 import { FormsModule }         from '@angular/forms';
 import { AppRoutingModule }    from './app-routing.module';
+import { HttpClientModule }    from '@angular/common/http';
 
 import { AppComponent }        from './app.component';
 import { HeroesComponent }     from './heroes/heroes.component';
@@ -20,7 +21,8 @@ import { DashboardComponent }  from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
