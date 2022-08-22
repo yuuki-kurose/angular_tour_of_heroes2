67a75b1a858af33948394293fb1c6684b640129f
yuuki kurose <pw1006@icloud.com>
2022-08-22 15:46:21 +0900

    feat: State内のセレクタ定義
    ヒーローの状態を知る為、HeroState内に状態を返すメソッドを設定する。

--- src/app/hero.state.ts
+++ src/app/hero.state.ts
@@ -13,10 +13,26 @@ export class HeroStateModel {
 }
 
 @State<HeroStateModel> ({
+  // nameはStateの名前を指す
   name:           'heroes',
+  // defaultsはHeroStateModelのデフォルト値を指す
   defaults: {
     selectedHero: null,
     heroes:       []
   }
 })
 
+export class HeroState {
+  // 依存性の注入
+  constructor(private heroService: HeroService) {}
+
+  // ここからセレクター↓
+  @Selector()
+    static heroes(state: HeroStateModel) {
+      return state.heroes;
+  }
+
+  @Selector()
+    static selectedHero(state: HeroStateModel) {
+      return state.selectedHero;
+  }
