29f5163948bd6b62b1a43a8959a49dca288205fa
yuuki kurose <pw1006@icloud.com>
2022-08-24 11:48:31 +0900

    feat: Receiverのメソッド定義
    Actionの代わりに、状態の変更を行うメソッドを設定する。

--- src/app/hero.receiver.ts
+++ src/app/hero.receiver.ts
@@ -1,10 +1,43 @@
-import { State, StateContext } from '@ngxs/store';
-import { receiver }            from '@ngxs-labs/emitter';
+import { Receiver } from '@ngxs-labs/emitter';
 
-import { HeroStateModel }      from './hero.state';
+import { Hero }     from './hero';
 
-@State<HeroStateModel> ({
-   name:    'heroes',
-   default: selectedHero: undefined,
-            heroes:       []
-})
+export namespace HeroAction {
+  
+  export const LOAD_HERO =   'Load_Hero';
+  export const SELECT_HERO = 'Select_Hero';
+  export const ADD_HERO =    'Add_Hero';
+  export const DELETE_HERO = 'Delete_Hero';
+  export const UPDATE_HERO = 'Update_Hero';
+
+  export class Load {
+    @Receiver()
+    public static readonly type = LOAD_HERO;
+  }
+
+  export class Select {
+    @Receiver()
+    public static readonly type = SELECT_HERO;
+    constructor(public id: number) { }
+  }
+
+  export class Add {
+    @Receiver()
+    public static readonly type = ADD_HERO;
+    constructor(public payload: Hero) { }
+  }
+
+  export class Delete {
+    @Receiver()
+    public static readonly type = DELETE_HERO;
+    constructor(public payload: Hero) { }
+  }
+
+  export class Update {
+    @Receiver()
+    public static readonly type = UPDATE_HERO;
+    constructor(public payload: Hero) { }
+  }
+}
+
+
