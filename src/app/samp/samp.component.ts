825d0aa8f16d4b71a7ee3331d16777c06e053fd5
yuuki kurose <pw1006@icloud.com>
2022-08-30 18:47:43 +0900

    refactor: 相対パスの修正
    パスが通っておらず、エラーになっていたので修正しました。

--- src/app/samp/samp.component.ts
+++ src/app/samp/samp.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector:    'app-samp',
   templateUrl: './samp.component.html',
-  styleUrls:   ['../Sass/Scss/_samp.component.css']
+  styleUrls:   ['../../Sass/Scss/_samp.component.scss']
 })
 export class SampComponent implements OnInit {
 
